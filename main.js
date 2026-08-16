3e823c4006008ee277625d9fa1871cd99cd6995f
Garry788-rk <rabozer1981@icloud.com>
2026-08-16 16:14:06 +0300

    Completing 5 points in JS

---
+++ main.js
@@ -0,0 +1,35 @@
+const products = document.querySelectorAll('.products__item');
+const changeColorAllBtn = document.querySelector('#change-Color-all-cards');
+const greenColorHash = '#4CAF50';
+const blueColorHash = '#2196F3';
+
+
+changeColorAllBtn.addEventListener('click', () => {
+    products.forEach((card) => card.style.backgroundColor = greenColorHash)
+      
+    });
+
+const firstProducts = document.querySelector('.products__item');
+const changeColorFirstBtn = document.querySelector('#change-Color-first-card');
+
+  changeColorFirstBtn.addEventListener('click', () => {
+    firstProducts.style.backgroundColor = blueColorHash;
+    });
+
+const openGoogleBtn = document.querySelector('#open-google');
+    openGoogleBtn.addEventListener('click',openGoogle)
+        window.open('https://www.google.com')
+
+function openGoogle() 
+{const answer = confirm('Вы хотите открыть страницу google.com?');
+    if (answer==true) {
+        window.open('https://www.google.com');
+    } else {return;}
+    }
+
+const outputConsoleLogBtn = document.querySelector('#output-console-log');
+outputConsoleLogBtn.addEventListener('click', () => (outputconsolelog('homework 6')));
+  function outputconsolelog(message) {  
+    alert(message) 
+    console.log(message)
+}
