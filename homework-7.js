7aba64de384e415f8dc21f0154b08a3de40d8223
Garry788-rk <rabozer1981@icloud.com>
2026-08-23 14:12:51 +0300

    completing all the points

--- homework-7.js
+++ homework-7.js
@@ -31,28 +31,39 @@ compareSpeed(3000000000);
 compareSpeed(false);
 compareSpeed("текст");
 
-let trainingCourse = "AI Engineering"
-let cost = 1000
+let trainingCourse = "AI Engineering";
+let cost = 1000;
 
 const budget = (currentBalance) => {
-
   if (currentBalance >= cost) {
-    console.log(
-      `Курс приобретен ${trainingCourse} Спасибо за покупку!`
-    );
-  }
-  else {
+    console.log(`Курс приобретен ${trainingCourse} Спасибо за покупку!`);
+  } else {
     let deficit = cost - currentBalance;
 
     console.log(
-      `Недостаточно средств для покупки курса ${trainingCourse}. Не хватает ${deficit}USD`
+      `Недостаточно средств для покупки курса ${trainingCourse}. Не хватает ${deficit}USD`,
     );
   }
-}
+};
 budget(1000);
 budget(500);
 
-
-  
-
-  
+function determineTheLevelOfDemandForTheProduct(score) {
+  if (score >= 10 && score <= 19) {
+    console.log("Низкий уровень спроса");
+  } else if (score >= 20 && score <= 49) {
+    console.log("Средний уровень спроса");
+  } else if (score >= 50 && score <= 79) {
+    console.log("Высокий уровень спроса");
+  } else if (score >= 80 && score <= 100) {
+    console.log("Очень высокий уровень спроса");
+  }
+  else {
+    console.log("Некорректные входные данные");
+  }
+}
+determineTheLevelOfDemandForTheProduct(0);
+determineTheLevelOfDemandForTheProduct(10);
+determineTheLevelOfDemandForTheProduct(30);
+determineTheLevelOfDemandForTheProduct(60);
+determineTheLevelOfDemandForTheProduct(90); 
