cdc85e81a4425afd3872e111fe98eb14ed61ed8a
Garry788-rk <rabozer1981@icloud.com>
2026-09-12 14:28:49 +0300

    making changes to improve code readability

--- homework-9.js
+++ homework-9.js
@@ -1,80 +1,79 @@
+import { comments } from "./comments.js";
+
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const filteredNumbers = numbers.filter((number) => number > 5);
+const clothes = ["hoodie", "t - shirt", "jeans", "jacket", "sneakers"];
+
 console.log(filteredNumbers);
 
-const reversedNumbers = numbers.reverse();
-function reversedNumbersArray(arr) {
+function reverseArray(arr) {
   if (!Array.isArray(arr)) {
     return "Incorrect data type. Please provide an array";
   }
+
   return [...arr].reverse();
 }
-console.log("reversedNumbers:", numbers);
 
-const clothes = ["hoodie", "t - shirt", "jeans", "jacket", "sneakers"];
+const reverseNumbers = reverseArray(numbers);
+const reverseClothes = reverseArray(clothes);
+
+console.log("reverseNumbers:", reverseNumbers);
+console.log("reverseClothes:", reverseClothes);
+
 function hasClothes(clothesName) {
   if (typeof clothesName !== "string") {
     return "Incorrect data type. Please provide a string";
   }
+
   return clothes.includes(clothesName);
 }
-const reversedClothes = clothes.reverse();
-function reversedClothesArray(arr) {
-  if (!Array.isArray(arr)) {
-    return "Incorrect data type. Please provide an array";
-  }
-  return [...arr].reverse();
-}
-console.log(hasClothes("jacket"));
-console.log(hasClothes("socks"));
-console.log(hasClothes(10));
-console.log("reversedClothes:", clothes);
 
-import { comments } from "./comments.js";
-console.log(comments);
+const hasJacket = hasClothes("jacket");
+const hasSocks = hasClothes("socks");
+
+console.log(hasJacket);
+console.log(hasSocks);
+console.log(hasClothes("10"));
 
 const comEmailCommeents = comments.filter((comment) =>
   comment.email.includes(".com"),
 );
 console.log(comEmailCommeents);
 
-const updatedComments = comments.map((comment) => {
-  const updatedComment = { ...comment };
-  if (updatedComment.id < 5) {
-    updatedComment.postId = 2;
-  } else {
-    updatedComment.postId = 1;
-  }
-  return updatedComment;
-});
+const updatedComments = comments.map((comment) => ({
+  ...comment,
+  postId: comment.id < 5 ? 2 : 1,
+}));
+
 console.log(updatedComments);
 
-const simplefiedComments = comments.map((comment) => ({
-  id: comment.id,
-  name: comment.name,
+const simplefiedComments = comments.map((comment) => ({ id, name }) => ({
+  id,
+  name,
 }));
+
 console.log(simplefiedComments);
 
-const validatedComments = comments.map((comment) => {
-  const updatedComment = { ...comment };
-  updatedComment.isInvalid = updatedComment.body.length > 180;
-  return updatedComment;
-});
+const validatedComments = comments.map((comment) => ({
+  ...comment,
+  isinvalid: comment.body.length > 180,
+}));
+
 console.log(validatedComments);
 
 const emailReduce = comments.reduce((accumulator, comment) => {
   accumulator.push(comment.email);
+
   return accumulator;
 }, []);
+
 console.log(emailReduce);
 
-const emailsMap = comments.map((comment) => comment.email);
-console.log(emailsMap);
-
-const emails = comments.map((comment) => comment.email);
+const emails = comments.map(({ email }) => email);
 const result1 = emails.toString();
 const result2 = emails.join(", ");
 const result3 = emails.join(" \\n ");
+
 console.log(result1);
 console.log(result2);
 console.log(result3);
